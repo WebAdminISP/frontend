c37623e2c162266ec583974306c72e9e9cc0a480
andreschunior <andres23540575@gmail.com>
2024-08-22 19:30:20 -0400

    actualizacion proyecto final, se hizo responsive el dashboard de usuario y de admin

--- proyectofinal-henry/src/components/Dashboard/Main/Agregarequipo/Agregarequipo.tsx
+++ proyectofinal-henry/src/components/Dashboard/Main/Agregarequipo/Agregarequipo.tsx
@@ -46,7 +46,7 @@ const Agregarequipo: React.FC = () => {
 
   return (
     <div
-      className={`p-3 mt-20 transition-all duration-1000  ${
+      className={`p-3 mt-20 transition-all duration-1000 ${
         btnFixed ? "ml-[270px]" : "ml-24"
       }`}
     >
